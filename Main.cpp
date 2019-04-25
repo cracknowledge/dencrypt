b1e8d6f7d477b9923a6a19b85d5bedcbd446b1ed
selenkutanoglu <selenkutanoglu@gmail.com>
2019-04-25 20:35:52 +0300

    additive cipher has been completed.

--- Main.cpp
+++ Main.cpp
@@ -1,56 +1,53 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <math.h>
 
+#define KEY 3
+
+using namespace std;
+char alphabetEn [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 /************** Additive Cipher **************/
 //Encryption : c = (p+k) mod n
 //Decryption : p = (c-k) mod n
 //n = alphabet length
 
-
 //This function returned position of the character on alphabet.
-int findPosition(char p,char alphabet[],int alphabetLength)
+int findPosition(char p,int alphabetLength)
 {
+
   for(int i=0;i<alphabetLength;i++)
   {
-    if(p == alphabet[i]) return i;
+    if(p == alphabetEn[i]) return i;
   }
   return -1; //Not find
 }
 
-void additiveCipherEncrypt(string plainText, int key, char alphabet[],int alphabetLength)
+void additiveCipherEncrypt(string plainText, int key,char alphabet[], int alphabetLength)
 {
-  int length = plainText.size(); //Length of plain text
+  int length = plainText.length(); //Length of plain text
   string cipherText = "";
-  
+  key=key%alphabetLength;
   for(int i=0;i<length;i++)
   {
-     int position = findPosition(plainText[i],alphabet,alphabetLength);
-     int cipherCharPos = (position + key) % alphabetLength;
-     cipherText += alphabet[cipherCharPos];
+     int position = findPosition(plainText[i],alphabetLength);
+     int cipherCharPos = (position + key +26) % alphabetLength;
+     cipherText += alphabetEn[cipherCharPos];
   }
-	cout<<cipherText;
+    cout<<cipherText;
 }
 
-void additiveCipherDecrypt(string cipherText,int key, char alphabet[], int n)
+void additiveCipherDecrypt(string cipherText,int key, char alphabet[])
 {
-  
+
 }
-
-
 /************** End Of Additive Cipher **************/
-  
+
 int main()
 {
-  char alphabetEn [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-  string plainText = "";
-  int key = 0;
-  cout<<"Enter the plaintext : ";
-  cin>>plainText;
-  cout<<endl<<"Enter the key value : ";
-  cin>>key;
-  additiveCipherEncrypt(plainText,key,alphabetEn,26);
-  
+  string var;
+  cout<<"Sifrelenecek metini giriniz : ";
+  cin>>var;
+  additiveCipherEncrypt(var,KEY,alphabetEn,26);
   return 0;
 }
